697b4b985aeba2cd4bcfcc46884aa2a0b7921dde
finn-01 <nguyensam576@gmail.com>
2022-02-08 00:36:05 +0700

    CTASections.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Example from "./Example";
 import Features from "./Features";
+import CTASections from "./CTASections";
 
 function App() {
 	return (
 		<>
-			<Example />
-			<Features />
+			<CTASections />
 		</>
 	);
 }
